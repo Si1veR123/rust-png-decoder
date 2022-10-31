9cb23ca3d763fcbf73658e2dd4b02dad2744a82f
Connor George <connorgeorgeem@aol.com>
2022-10-31 22:28:17 +0000

    Initial wasm

---
+++ src/token.rs
@@ -0,0 +1,7 @@
+
+struct Token {
+    bytes: Vec<u8>,
+    nest_level: u8,
+    data: String,
+    name: String,
+}
---
+++ src/wasm_bindings.rs
@@ -0,0 +1,8 @@
+
+use wasm_bindgen::prelude::*;
+use main::*;
+
+#[wasm_bindgen]
+pub fn decode_png(data: Vec<u8>) {
+    
+}
